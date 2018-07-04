572929f37b92ce9ad0eaa1fda3784fc2201d4490
Vaclav Synacek <vaclav@synacek.com>
2018-07-04 13:24:49 +0200

    naive implementation works

--- src/htmlparser2hiccup/core.cljs
+++ src/htmlparser2hiccup/core.cljs
@@ -1,23 +1,31 @@
 (ns htmlparser2hiccup.core
-  (:require [htmlparser2 :as h2]))
+  (:require [htmlparser2 :as h2]
+            [clojure.string :as string]))
 
 (enable-console-print!)
 
-(defn- open-tag
-  [tag attrs]
-  (println "TAG: " tag (js->clj attrs :keywordize-keys true)))
 
-(defn- close-tag
-  [tag]
-  (println "END: " tag))
+(defn html->hiccup
+  [html-text]
+  (let [result-accumulator (goog.string/StringBuffer.)
+        open-tag (fn [tag attrs]
+                     (.append result-accumulator
+                              " [:"
+                              tag
+                              " "
+                              (js->clj attrs :keywordize-keys true)))
+        close-tag (fn [tag]
+                     (.append result-accumulator
+                              "]"))
+        text (fn [text]
+                     (.append result-accumulator
+                              " "
+                              (pr-str text)))
+        parser (h2/Parser. #js {"onopentag" open-tag
+                               "onclosetag" close-tag
+                               "ontext" text})]
+        (.write parser html-text)
+        (string/trim (str result-accumulator))))
 
-(defn- text
-  [text]
-  (println "TEXT: " text))
-
-(def parser (h2/Parser. #js {"onopentag" open-tag
-                             "onclosetag" close-tag
-                             "ontext" text}))
-
-(.write parser "<p>bla <a href=\"http://example.com\">link</a></p>")
-
+(def t "<p>bla<a href=\"http://example.com\">link</a></p>")
+(println (html->hiccup t))
